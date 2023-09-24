64c947e161dbd47eb19caf59af9964f05a5526d1
Agam Singh <120456245+DreamFaang78@users.noreply.github.com>
2023-09-24 10:10:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+$("h1").addClass("big-title margin-50");
+$("h1").text("Bye Bye");
+$("button").html("<em>Let's Click Me</em>");
+console.log($("img").attr("src",));
+$("a").attr("href", "https://www.yahoo.com");
+//Adding Event Listener
+$("h1").click(function(){
+    $("h1").css("color", "purple");
+});
+// 
+$("button").click(function(){
+    $("h1").css("color", "purple");
+});
+$("input").keypress(function(event){
+    console.log(event.key);
+});
+$("h1").on("mouseover", function(){ 
+    $("h1").css("color", "purple");
+});
+$(document).keypress(function(event){
+    $("h1").text(event.key);
+});
